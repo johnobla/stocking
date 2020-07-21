5551e942bf95c647ef4fc5a0bc96813a4fa0df1b
johnobla <45395888+johnobla@users.noreply.github.com>
2020-07-21 12:25:10 +0100

    Installed devise

--- Gemfile
+++ Gemfile
@@ -16,15 +16,19 @@ gem 'jbuilder', '~> 2.5'
 gem 'sassc', '~> 2.1.0'
 gem 'bootstrap', '~> 4.5'
 gem 'jquery-rails'
+gem 'stock_quote', '~> 3.0'
+gem 'devise', '~> 4.7', '>= 4.7.2'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'capybara', '>= 2.15'
   gem 'selenium-webdriver'
-end
+ 
 
 group :development do
   gem 'web-console', '>= 3.3.0'
+  gem 'prettier'
+  gem 'rubocop', '~> 0.59.2'end
 end
 
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,23 @@
 class HomeController < ApplicationController
   def index
+    @api = StockQuote::Stock.new(api_key: 'sk_2e04c119ff1147a0be87e348a037e038')
+
+    if params[:ticker] == ""
+
+      @nothing = "Hey! You forgot to enter a symbol!"
+
+    elsif params[:ticker]
+
+      begin
+        @stock = StockQuote::Stock.quote(params[:ticker])
+      rescue RuntimeError, NoMethodError
+        @error = "Please enter a valid symbol!"
+      # rescue 
+      #   @error = "Please enter a valid symbol!"
+      end
+
+    end
+    
   end
 
   def about
